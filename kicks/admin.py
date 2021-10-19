ef4f5e18d3339b01541683d9e701fa8a709f39f1
RaVeR76 <leefoster76@live.co.uk>
2021-10-19 18:07:54 +0000

    Remove 's' from Kickss in Admin & change KicksType to Type

---
+++ kicks/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Kicks, Brand, Type
+
+# Register your models here.
+admin.site.register(Kicks)
+admin.site.register(Brand)
+admin.site.register(Type)
---
+++ kicks/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from common.models import Category, Colour, Size
+
+
+class Kicks(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Kicks'
+
+    category = models.ForeignKey('common.Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    kicks_type = models.ForeignKey('Type', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    colour = models.ForeignKey('common.Colour', null=True, blank=True, on_delete=models.SET_NULL)
+    sizes_available = models.ForeignKey('common.Size', null=True, blank=True, on_delete=models.SET_NULL)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name

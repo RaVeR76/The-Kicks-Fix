25f7860385987bfc5845cac2ef6dbf0b11cbd102
RaVeR76 <leefoster76@live.co.uk>
2021-10-28 20:57:27 +0000

    Fix accessories views error & add link to navbar

--- accessories/views.py
+++ accessories/views.py
@@ -3,13 +3,14 @@ from .models import Accessories
 
 # Create your views here.
 
+
 def all_accessories(request):
     """ A view to show all kaccessories, including sorting and search queries """
 
     accessories = Accessories.objects.all()
 
     context = {
-        'accessories': all_accessories,
+        'accessories': accessories,
     }
 
     return render(request, 'accessories/accessories.html', context)

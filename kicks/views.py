e164e775f881b7e6a8d2d1fcef0a5d6a80abc0c0
RaVeR76 <leefoster76@live.co.uk>
2021-10-29 18:12:26 +0000

    Add Category query functionality & test

--- kicks/views.py
+++ kicks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Kicks
+from common.models import Category
 
 # Create your views here.
 
@@ -11,20 +12,27 @@ def all_kicks(request):
 
     kicks = Kicks.objects.all()
     query = None
+    category = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            kicks = kicks.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-            
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             kicks = kicks.filter(queries)
 
     context = {
         'kicks': kicks,
         'search_word': query,
+       # 'chosen_categories': categories,
     }
 
     return render(request, 'kicks/kicks.html', context)

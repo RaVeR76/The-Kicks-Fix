e4cf2d284dcc237703b1ed043acb5f7689334501
RaVeR76 <leefoster76@live.co.uk>
2021-11-06 23:03:29 +0000

    Add to context processor & fix bag items bug

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,19 +1,41 @@
 from decimal import Decimal
-from django.conf import settings
+from django.shortcuts import get_object_or_404
 from home.models import Discount
+from kicks.models import Kicks
+from accessories.models import Accessories
 
 
 def bag_contents(request):
 
     site_discounts = Discount.objects.get(name='discount') # Utilise Django admin for discounts & delivery costs
-
     bag_items = []
     total = 0
     kicks_count = 0
     accessories_count = 0
+    bag = request.session.get('bag', {})
 
     free_delivery_threshold = site_discounts.free_delivery_threshold
     standard_delivery_percentage = site_discounts.standard_delivery_percentage
+
+    for item_id, quantity in bag.items():
+        if "RAVE" not in item_id: # If No RAVE then it's a Kicks Item
+            kicks = get_object_or_404(Kicks, sku=item_id)
+            total += quantity * kicks.price
+            kicks_count += quantity
+            bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'kicks': kicks,
+            })
+        elif "RAVE" in item_id: # If RAVE then it's an Accessories Item
+            accessories = get_object_or_404(Accessories, sku=item_id)
+            total += quantity * accessories.price
+            accessories_count += quantity
+            bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'accessories': accessories,
+            })
 
     if total < free_delivery_threshold:
         delivery = total * standard_delivery_percentage / 100

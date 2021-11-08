47ee7bb5b8a6dfb834019bb438688ed5af8c026d
RaVeR76 <leefoster76@live.co.uk>
2021-11-08 16:59:13 +0000

    Refactor bag.items code in context.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,8 +10,7 @@ def bag_contents(request):
     site_discounts = Discount.objects.get(name='discount') # Utilise Django admin for discounts & delivery costs
     bag_items = []
     total = 0
-    kicks_count = 0
-    accessories_count = 0
+    product_count = 0
     bag = request.session.get('bag', {})
 
     free_delivery_threshold = site_discounts.free_delivery_threshold
@@ -20,22 +19,25 @@ def bag_contents(request):
     for item_id, quantity in bag.items():
         if "RAVE" not in item_id: # If No RAVE then it's a Kicks Item
             product = get_object_or_404(Kicks, sku=item_id)
-            total += quantity * product.price
-            kicks_count += quantity
-            bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-            })
         elif "RAVE" in item_id: # If RAVE then it's an Accessories Item
             product = get_object_or_404(Accessories, sku=item_id)
-            total += quantity * product.price
-            accessories_count += quantity
-            bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-            })
+        total += quantity * product.price
+        product_count += quantity
+        bag_items.append({
+        'item_id': item_id,
+        'quantity': quantity,
+        'product': product,
+        })
+
+     #   elif "RAVE" in item_id: # If RAVE then it's an Accessories Item
+      #      product = get_object_or_404(Accessories, sku=item_id)
+         #   total += quantity * product.price
+         #   accessories_count += quantity
+         #   bag_items.append({
+         #   'item_id': item_id,
+         #   'quantity': quantity,
+         #   'product': product,
+         #   })
 
     if total < free_delivery_threshold:
         delivery = total * standard_delivery_percentage / 100
@@ -49,8 +51,7 @@ def bag_contents(request):
     context = {
         'bag_items': bag_items,
         'total': total,
-        'kicks_count': kicks_count,
-        'accessories_count': accessories_count,
+        'product_count': product_count,
         'delivery': delivery,
         'free_delivery_topup': free_delivery_topup,
         'free_delivery_threshold': free_delivery_threshold,

3a30a64c44ecbabcf287c0af0f3464c5566f3942
RaVeR76 <leefoster76@live.co.uk>
2021-11-09 22:41:14 +0000

    Add female sizes in kicks views & JS for selections

--- kicks/views.py
+++ kicks/views.py
@@ -127,6 +127,11 @@ def kicks_detail(request, kicks_id):
     kids_sizes_eu = kids_sizes.size.get("eu")
     kids_sizes_us = kids_sizes.size.get("us")
 
+    women_sizes = Size.objects.get(name='women_sizes')
+    women_sizes_uk = women_sizes.size.get("uk")
+    women_sizes_eu = women_sizes.size.get("eu")
+    women_sizes_us = women_sizes.size.get("us")
+
    # kids_sizes_uk = [10.5, 11.5, 12.5, 13.5, 1.5, 2.5, 3.5, 4, 4.5]
 
    # women_sizes_uk = [3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5]
@@ -142,6 +147,9 @@ def kicks_detail(request, kicks_id):
         'kids_sizes_uk': kids_sizes_uk,
         'kids_sizes_eu': kids_sizes_eu,
         'kids_sizes_us': kids_sizes_us,
+        'women_sizes_uk': women_sizes_uk,
+        'women_sizes_eu': women_sizes_eu,
+        'women_sizes_us': women_sizes_us,
     }
 
     return render(request, 'kicks/kicks_detail.html', context)

93504a4cb1d89a230b10789fbe65904d60e12281
RaVeR76 <leefoster76@live.co.uk>
2021-11-12 15:56:16 +0000

    Fix update Kicks Bug & add Remove Item functionality

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -43,24 +43,21 @@ def add_to_bag(request, item_id):
 
 
 def update_bag(request, item_id):
-    """ Update the quantity of selected item to the shopping bag by the selected amount"""
+    """ Update the quantity of selected item to the shopping bag by the selected amount """
 
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
     size = None
-
-    if 'kids_size' in request.POST:
-        size = request.POST['kids_size']
-    if 'female_size' in request.POST:
-        size = request.POST['female_size']
-    if 'male_size' in request.POST:
-        size = request.POST['male_size']
+    if 'kicks_size' in request.POST:
+        size = request.POST['kicks_size']
 
     if size:
         if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
+            bag[item_id]['kicks_by_size'][size] = quantity
         else:
-            del bag[item_id]['items_by_size'][size]
+            del bag[item_id]['kicks_by_size'][size]
+            if not bag[item_id]['kicks_by_size']:
+                bag.pop(item_id)
     else:
         if quantity > 0:
             bag[item_id] = quantity
@@ -69,3 +66,26 @@ def update_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """ Remove item from the bag """
+
+    try:
+        bag = request.session.get('bag', {})
+        size = None
+        if 'kicks_size' in request.POST:
+            size = request.POST['kicks_size']
+
+        if size:
+            del bag[item_id]['kicks_by_size'][size]
+            if not bag[item_id]['kicks_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)

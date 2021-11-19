69834dc70f6baabe041b73867434b084055f8875
RaVeR76 <leefoster76@live.co.uk>
2021-11-19 23:47:14 +0000

    Create UserProfile form, profile page & order history page

--- profiles/views.py
+++ profiles/views.py
@@ -1,16 +1,36 @@
-
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request):
+    """ Display the user's delevery details & order history. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
+    template = 'profiles/order_history.html'
+    context = {
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)

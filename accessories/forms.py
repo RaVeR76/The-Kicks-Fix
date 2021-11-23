d74c72ec02236388b0405a27881c82c64dbfc357
RaVeR76 <leefoster76@live.co.uk>
2021-11-23 17:37:17 +0000

    Store Admin - Add Accessories Form

---
+++ accessories/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import Accessories, Type
+from common.models import Category, Colour
+
+
+class AccessoriesForm(forms.ModelForm):
+
+    class Meta:
+        model = Accessories
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all().exclude(name='kicks')
+        cat_friend_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        type = Type.objects.all()
+        type_friend_names = [(t.id, t.get_friendly_name()) for t in type]
+
+        colour = Colour.objects.all()
+        colour_friend_names = [(c.id, c.get_friendly_name()) for c in colour]
+
+
+        self.fields['category'].choices = cat_friend_names
+        self.fields['type'].choices = type_friend_names
+        self.fields['colour'].choices = colour_friend_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'

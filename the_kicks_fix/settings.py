c25bfe131c565ff44f9cd381f86911b4c3ace62f
RaVeR76 <leefoster76@live.co.uk>
2021-11-25 15:31:00 +0000

    Removed secret key & set debug

--- the_kicks_fix/settings.py
+++ the_kicks_fix/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fq@(e1j^a(kfhs^_*wy-my^d0zl(9&vaud+$f!(jruc#0(jpy^'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['raver76-the-kicks-fix.herokuapp.com', 'localhost']
 
